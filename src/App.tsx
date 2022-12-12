e352824620ebf899af10cbf480f82fcc9d66cc71
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-11 21:07:23 -0600

    Add routes to navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.scss';
+import Game from './components/TicTacToe/Game/Game';
+import ColorsMatrix from './components/ColorsMatrix/ColorsMatrix';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 
 function App(): React.ReactElement {
-  return <div className='App'>Hola Mundo</div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/colors-matrix' element={<ColorsMatrix />} />
+        <Route path='/tic-tac-toe' element={<Game />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
