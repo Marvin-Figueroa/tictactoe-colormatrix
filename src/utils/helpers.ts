025fd73b06bd114e8fa7ac0cebd1f3b2a50a10e5
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-12 20:42:45 -0600

    Add calculateWinner helper function for the tictactoe

---
+++ src/utils/helpers.ts
@@ -0,0 +1,25 @@
+export function calculateWinner(squares: Array<string | null>): string | null {
+  const winninCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winninCombinations.length; i++) {
+    const [a, b, c] = winninCombinations[i];
+
+    if (
+      squares[a] !== null &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
+}
