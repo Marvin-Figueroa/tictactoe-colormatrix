ca1296ee46eca905270f01b4ef16dd2c43f73e97
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-12 20:49:21 -0600

    Remove unused eslint-disable comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.scss';
 import Game from './components/TicTacToe/Game/Game';
 import ColorsMatrix from './components/ColorsMatrix/ColorsMatrix';
